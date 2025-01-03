7b7bcb256546e105cecc03d1f5a719957971221c
Haroon <haroonasifmalik123@gmail.com>
2025-01-03 19:44:37 +0500

    feat(): add defalt value for image field

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,26 @@
+import random
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.deconstruct import deconstructible
 from django_extensions.db.models import TimeStampedModel
 
 from accounts.managers import CustomUserManager
+
+
+@deconstructible
+class RandomAvatar:
+    def __init__(self):
+        self.avatar = [
+            "avatars/1.png",
+            "avatars/2.png",
+            "avatars/3.png",
+            "avatars/4.png",
+            "avatars/5.png",
+        ]
+
+    def __call__(self):
+        return random.choice(self.avatar)
 
 
 class CustomUser(AbstractUser, TimeStampedModel):
@@ -18,7 +36,9 @@ class CustomUser(AbstractUser, TimeStampedModel):
 
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=255, blank=True, null=True)
-    image = models.ImageField(upload_to='user_images/', blank=True, null=True)
+    image = models.ImageField(
+        upload_to="user_images/", blank=True, null=True, default=RandomAvatar()
+    )
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
